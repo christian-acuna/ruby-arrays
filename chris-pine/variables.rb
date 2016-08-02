05200951057fdc5ddebd8fe4ab1c9230bd2f4837
Christian Acuna <cacuna0828@gmail.com>
2016-08-02 10:25:22 +0800

    finish chapter 3 variables and assignment

---
+++ chris-pine/variables.rb
@@ -0,0 +1,37 @@
+# my_string = '...you can say that again...'
+#
+# puts my_string
+# puts my_string
+#
+# name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+# puts 'My name is ' + name + '.'
+# puts 'Wow! ' + name + ' is a really long name!'
+#
+# composer = 'Mozart'
+# puts composer + ' was "da bomb", in his day.'
+#
+# composer = 'Beethoven'
+# puts 'But I prefer ' + composer + ', personally.'
+#
+# var = 'just another ' + 'string'
+# puts var
+#
+# var = 5 * (1 + 2)
+# puts var
+#
+# var1 = 8
+# var2 = var1 # 8
+#
+# puts var1
+# puts var2
+#
+# puts ''
+#
+# var1 = 'eight'
+# puts var1
+# puts var2
+
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+puts var1 + var2.to_i
